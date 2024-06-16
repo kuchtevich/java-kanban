de29ddcadcdb63d136124ec475fd1badfb91216e
User Daria <kuchtevich@yandex.tu>
2024-06-16 20:43:23 +0300

    ТЗ-8 добавление времени для задач

--- test/service/FileBackedTaskManagerTest.java
+++ test/service/FileBackedTaskManagerTest.java
@@ -40,10 +40,12 @@ class FileBackedTaskManagerTest {
     public void shouldBeGetInformationInFile() throws IOException {
         FileBackedTaskManager fileBackedTaskManager = new FileBackedTaskManager(file);
         Task task = new Task("Cоздание новой задачи",
-                "сменить профессию", Status.NEW, LocalDateTime.of(2024, 12, 12, 12, 12), Duration.ofMinutes(600000));
+                "сменить профессию", Status.NEW,
+                LocalDateTime.of(2024, 12, 12, 12, 12), Duration.ofMinutes(600000));
         Epic epic = new Epic("Создание нового эпика", "учеба");
         SubTask subTask = new SubTask("Создание новой подзадачи",
-                "учеба", Status.NEW, LocalDateTime.of(2024, 12, 12, 12, 12), Duration.ofMinutes(600000), 2);
+                "учеба", Status.NEW,
+                LocalDateTime.of(2024, 12, 12, 12, 12), Duration.ofMinutes(600000), 2);
         fileBackedTaskManager.addNewTask(task);
         fileBackedTaskManager.addNewEpic(epic);
         fileBackedTaskManager.addNewSubtask(subTask);
@@ -53,82 +55,5 @@ class FileBackedTaskManagerTest {
         assertEquals(fileBackedTaskManager.getAllSubTasks(), fileBackedTaskManager2.getAllSubTasks(), "Ошибка");
     }
 
-    @Test
-    public void shouldWriteTasksInFile() throws IOException {
-        Task task = new Task("task1", "description", Status.NEW,
-                LocalDateTime.of(2024, 12, 12, 12, 12), Duration.ofMinutes(600000));
-        taskManager.addNewTask(task);
-
-        Epic epic = new Epic("epic1", "description");
-        taskManager.addNewEpic(epic);
-
-        SubTask subTask = new SubTask("subtask1", "description", Status.NEW,
-                LocalDateTime.of(2024, 12, 12, 12, 12), Duration.ofMinutes(600000), 2);
-        taskManager.addNewSubtask(subTask);
-
-        String fr = Files.readString(file.toPath());
-        String[] lines = fr.split(";");
-
-        assertEquals(4, lines.length, "Ошибка");
-    }
-
-    @Test
-    public void shouldWriteAndDeleteTasksInFileTest() throws IOException {
-        Task task = new Task("task1", "description", Status.NEW,
-                LocalDateTime.of(2024, 12, 12, 12, 12), Duration.ofMinutes(600000));
-        taskManager.addNewTask(task);
-        Task task2 = new Task("task2", "description", Status.NEW,
-                LocalDateTime.of(2024, 12, 12, 12, 12), Duration.ofMinutes(600000));
-        taskManager.addNewTask(task2);
-        taskManager.deleteTask(2);
-
-        Epic epic = new Epic("epic1", "description");
-        taskManager.addNewEpic(epic);
-        taskManager.removeAllEpics();
-
-        FileBackedTaskManager f2 = FileBackedTaskManager.loadFromFile(file);
-
-        assertEquals(1, taskManager.getAllTasks(), "Удаление неверно");
-        assertEquals(0, taskManager.getAllEpics(), "Удаление неверно");
-    }
-
-    @Test
-    public void shouldCreateTaskAndSaveFromFile() {
-        Task task = new Task("task1", "description", Status.NEW,
-                LocalDateTime.of(2024, 12, 12, 12, 12), Duration.ofMinutes(6000));
-        taskManager.addNewTask(task);
-
-        assertNotNull(taskManager.getHistory(), "Пусто");
-        assertNotNull(taskManager.getAllTasks(), "Пусто");
-    }
-
-    @Test
-    public void shouldTimeTest() {
-        Task task = new Task("task1", "description", Status.NEW,
-                LocalDateTime.of(2024, 12, 12, 12, 12), Duration.ofMinutes(600));
-        taskManager.addNewTask(task);
-
-        Epic epic = new Epic("Новый эпик1", "Описание");
-        taskManager.addNewEpic(epic);
-
-        SubTask subTask = new SubTask("Новая подзадача1", "Описание", Status.NEW,
-                LocalDateTime.of(2024, 12, 12, 12, 12), Duration.ofMinutes(6000), 3);
-        taskManager.addNewSubtask(subTask);
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy_HH:mm");
-
-        Epic epicTest = taskManager.getEpic(2);
-        String startTimeEpic = epicTest.getStartTime().format(formatter);
-        String endTimeEpic = epicTest.getEndTime().format(formatter);
-
-        Duration durationEpic = epicTest.getDuration();
-        LocalDateTime start = subTask.getStartTime();
-        LocalDateTime finish = subTask.getEndTime();
-        Duration duration = Duration.between(start, finish);
-
-        assertEquals("16.06.2024_14:44", startTimeEpic, "Ошибка");
-        assertEquals("16.06.2024_14:45", endTimeEpic, "Ошибка");
-        assertEquals(duration, durationEpic, "Ошибка");
-    }
 
 }
